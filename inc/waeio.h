267e141779b29a21401ff5b3fbe36423a4464ad2
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2024-04-03 15:46:45 +0200

    Redefine interface

--- inc/waeio.h
+++ inc/waeio.h
@@ -1,6 +1,7 @@
 #ifndef WAEIO_H
 #define WAEIO_H
 
+#include <stdint.h>
 #include <wasio.h>
 #include <wasm_utils.h>
 
@@ -8,18 +9,18 @@ __wasm_export__("waeio_main")
 int waeio_main(void* (*main)(void*), void*);
 
 __wasm_export__("waeio_async")
-int waeio_async(void *(*proc)(void*), void*);
+int waeio_async(void *(*proc)(struct wasio_fd*), struct wasio_fd *fd);
 
 __wasm_export__("waeio_accept")
-int waeio_accept(int fd);
+int waeio_accept(struct wasio_fd fd, struct wasio_fd *new_conn);
 
 __wasm_export__("waeio_recv")
-int waeio_recv(int fd, char *buf, size_t len);
+int waeio_recv(struct wasio_fd fd, uint8_t *buf, uint32_t len, uint32_t *recvlen);
 
 __wasm_export__("waeio_send")
-int waeio_send(int fd, char *buf, size_t len);
+int waeio_send(struct wasio_fd fd, uint8_t *buf, uint32_t len, uint32_t *sendlen);
 
 __wasm_export__("waeio_close")
-int waeio_close(int fd);
+int waeio_close(struct wasio_fd fd);
 
 #endif
