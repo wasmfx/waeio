7727357137a9f4eb4701e959fb00ae80d98cbf1f
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2024-04-15 15:35:57 +0200

    Update hello example, illustrating writing to stack (interior) memory, heap memory, and static memory from the host.

--- examples/hello/hello.c
+++ examples/hello/hello.c
@@ -8,13 +8,25 @@ extern
 __wasm_import__("host", "put_hello")
 void put_hello(uint8_t *buf, uint32_t buf_len);
 
+static char global[12] = "xxxxx yyyyy";
+
 int main(void) {
   const size_t buf_len = strlen("hello world");
-  //uint8_t *buf = (uint8_t*)malloc(buf_len);
-  uint8_t buf[buf_len];
-  memset(buf, '\0', buf_len);
-  put_hello(buf, buf_len);
-  printf("%s\n", (char*)buf);
-  //free(buf);
+
+  // Stack allocation.
+  uint8_t stk[buf_len]; // stack allocated.
+  memset(stk, '\0', buf_len);
+  put_hello(stk, buf_len);
+  printf(" stack: %s\n", (char*)stk);
+
+  // Heap allocation.
+  uint8_t *heap = (uint8_t*)malloc(sizeof(uint8_t)*buf_len);
+  put_hello(heap, buf_len);
+  printf("  heap: %s\n", (char*)heap);
+  free(heap);
+
+  // Global/static allocation.
+  put_hello((uint8_t*)global, buf_len);
+  printf("static: %s\n", (char*)global);
   return 0;
 }
